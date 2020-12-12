be37612d10b8da23dc28010e13e027899441606a
lakoc <xpolok03@stud.fit.vutbr.cz>
2020-12-12 16:27:16 +0100

    day 12

---
+++ day12/part1.py
@@ -0,0 +1,39 @@
+def move_in_direction(direction_to_move, value):
+    if direction_to_move == 'E':
+        position[0] += value
+    elif direction_to_move == 'N':
+        position[1] += value
+    elif direction_to_move == 'W':
+        position[0] -= value
+    elif direction_to_move == 'S':
+        position[1] -= value
+
+
+def change_direction(angle):
+    directions = ['N', 'E', 'S', 'W']
+    current_position = directions.index(direction)
+    new_dir_diff = int(angle / 90)
+    return directions[(current_position + new_dir_diff) % 4]
+
+
+with open('day12/input') as file:
+    lines = file.readlines()
+    direction = 'E'
+    position = [0, 0]
+    for line in lines:
+        line = line.rstrip()
+        if line[0] == 'F':
+            move_in_direction(direction, int(line[1:]))
+        elif line[0] == 'N':
+            move_in_direction(line[0], int(line[1:]))
+        elif line[0] == 'S':
+            move_in_direction(line[0], int(line[1:]))
+        elif line[0] == 'W':
+            move_in_direction(line[0], int(line[1:]))
+        elif line[0] == 'E':
+            move_in_direction(line[0], int(line[1:]))
+        elif line[0] == 'L':
+            direction = change_direction(-int(line[1:]))
+        elif line[0] == 'R':
+            direction = change_direction(int(line[1:]))
+    print(abs(position[0]) + abs(position[1]))
---
+++ day12/part2.py
@@ -0,0 +1,27 @@
+with open('day12/input') as file:
+    directions = ['N', 'E', 'S', 'W']
+    lines = file.readlines()
+    position = [0, 0]
+    waypoint_dir = 1
+    waypoint = [10, 1]
+    for line in lines:
+        line = line.rstrip()
+        n = int(line[1:])
+        if line[0] == 'F':
+            position[0] += n * waypoint[0]
+            position[1] += n * waypoint[1]
+        elif line[0] == 'N':
+            waypoint[1] += n
+        elif line[0] == 'S':
+            waypoint[1] -= n
+        elif line[0] == 'E':
+            waypoint[0] += n
+        elif line[0] == 'W':
+            waypoint[0] -= n
+        elif line[0] == 'L':
+            for _ in range(n // 90):
+                waypoint[0], waypoint[1] = -waypoint[1], waypoint[0]
+        elif line[0] == 'R':
+            for _ in range(n // 90):
+                waypoint[0], waypoint[1] = waypoint[1], -waypoint[0]
+    print(abs(position[0]) + abs(position[1]))

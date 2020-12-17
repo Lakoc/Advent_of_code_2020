8c63ea0a5c745e2ea765cf0daabebf95821f0259
lakoc <xpolok03@stud.fit.vutbr.cz>
2020-12-17 17:18:51 +0100

    day 17

---
+++ day17/part1.py
@@ -0,0 +1,27 @@
+with open('input') as file:
+    lines = [line.strip() for line in file.readlines()]
+    active = set()
+    for i, line in enumerate(lines):
+        for j, char in enumerate(line):
+            if char == '#':
+                active.add((i, j, 0))
+
+    for step in range(0, 6):
+        new_active = set()
+        for x in range(-15, 15):
+            for y in range(-15, 15):
+                for z in range(-15, 15):
+                    active_neighbors = 0
+                    for dx in [-1, 0, 1]:
+                        for dy in [-1, 0, 1]:
+                            for dz in [-1, 0, 1]:
+                                if dx == 0 and dy == 0 and dz == 0:
+                                    continue
+                                if (x + dx, y + dy, z + dz) in active:
+                                    active_neighbors += 1
+                    if (x, y, z) in active and (active_neighbors == 2 or active_neighbors == 3):
+                        new_active.add((x, y, z))
+                    elif (x, y, z) not in active and active_neighbors == 3:
+                        new_active.add((x, y, z))
+        active = new_active
+    print(len(active))
